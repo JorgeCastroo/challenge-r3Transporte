6193510f45b20d1e77c95bf918db55f9d83ae54b
jorgecastro <jorgeeduardo27007@gmail.com>
2023-10-03 16:26:33 -0300

    feat: :rocket: Config Deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import * as path from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import * as path from 'path';
 
 export default defineConfig({
-  resolve:{
-      alias: {
-        "@": path.resolve(__dirname, "./src/"),
-      }
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src/'),
+    },
   },
   plugins: [react()],
-})
+  base: '/challenge-r3Transporte',
+});
